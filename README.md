69f09384f79be504496642df275d9cd99a034fd2
Alex Cornejo <acornejo@gmail.com>
2013-11-28 13:00:51 -0500

    Initial import.

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-knockout-extras
-===============
+# Knockout Extras
 
-Collection of knockout utilities.
+Includes several knockout extra's that I've found useful.
+
+## Dirty Flag
+
+For the most part it should be compatible with the dirtyFlag provided
+with kolite, or the smart dirty flag of Ryan Niemayer.
+
+## Basic Observables
+
+- observableInteger
+- observableNumber
+- observableDate
+
+These observables prevent your input from being cast to a string, and
+instead they remain an intenger, number or date (respectively). In
+addition, the observableDate provides sub observables for tying day,
+month or year (ideal for select boxes). 
+
+## Compound Observables
+
+- observableModel
+
+An observableModel is similar in spirit to the functionality provided by
+the knockout mapping plugin. The main difference is that an
+observableModel will preserve the type of each property of a model by
+using specialized observables (i.e. observableInteger, observableDate,
+        etc.). Therefore, an observableModel requires that a basic type
+schema is provided when creating the observable.
+
+In addition, it provides:
+
+- A dirtyFlag that tracks the state of the object.
+- An errors observableArray on each property, to keep track of
+validation errors, and a parseErrors method to update it.
---
+++ knockout.extras.js
@@ -0,0 +1,182 @@
+(function() {
+  function factory(ko, koext) {
+    koext.dirtyFlag = function(root, isInitiallyDirty) {
+      var _result = function () {},
+          _initialState = ko.observable(ko.toJSON(root)),
+          _isInitiallyDirty = ko.observable(isInitiallyDirty);
+
+      _result.isDirty = ko.computed(function () {
+        return _isInitiallyDirty() || _initialState() !== ko.toJSON(root);
+      });
+
+      _result.forceDirty =  function () {
+        _isInitiallyDirty(true);
+      };
+
+      _result.reset = function () {
+        _initialState(ko.toJSON(root));
+        _isInitiallyDirty(false);
+      };
+
+      return _result;
+    };
+
+    kext.observableDate = function (initialValue) {
+      var _actual = ko.observable(initialValue);
+
+      var _day = ko.observable(),
+          _month = ko.observable(),
+          _year = ko.observable();
+
+      function updateDate() {
+        var day = _day(), month = _month(), year = _year();
+        if (day !== undefined && month !== undefined && year !== undefined) 
+          _actual(new Date(year, month, day));
+        else if (day === undefined && month === undefined && year === undefined)
+          _actual(undefined);
+      }
+
+      _day.subscribe(updateDate);
+      _month.subscribe(updateDate);
+      _year.subscribe(updateDate);
+
+      var result = ko.dependentObservable({
+        read: function () {
+          return _actual();
+        },
+        write: function (newValue) {
+          var parsedValue = new Date(Date.parse(newValue));
+          if (isNaN(parsedValue.valueOf())) {
+            _actual(newValue)
+            _day(undefined);
+            _month(undefined);
+            _year(undefined);
+          }
+          else {
+            _day(parsedValue.getDate());
+            _month(parsedValue.getMonth());
+            _year(parsedValue.getFullYear());
+            _actual(parsedValue);
+          }
+        }
+      });
+
+      result.day = _day;
+      result.month = _month;
+      result.year = _year;
+
+      return result;
+    };
+
+    koext.observableInteger = function(initialValue) {
+      var _actual = ko.observable(initialValue);
+
+      var result = ko.dependentObservable({
+        read: function() {
+          return _actual();
+        },
+        write: function(newValue) {
+          var parsedValue = parseInt(newValue);
+          _actual(isNaN(parsedValue) ? newValue : parsedValue);
+        }
+      });
+
+      return result;
+    }
+
+    koext.observableNumber = function(initialValue) {
+      var _actual = ko.observable(initialValue);
+
+      var result = ko.dependentObservable({
+        read: function() {
+          return _actual();
+        },
+        write: function(newValue) {
+          var parsedValue = parseFloat(newValue);
+          _actual(isNaN(parsedValue) ? newValue : parsedValue);
+        }
+      });
+
+      return result;
+    };
+
+    koext.observableModel = function (model, initialValue) {
+      var _value = initialValue || {};
+      var _fields = {};
+
+      var result = ko.computed({
+        read: function() {
+          return ko.toJS(_fields);
+        },
+        write: function (newValue) {
+          for (var p in model) {
+            if (model.hasOwnProperty(p)) {
+              if (newValue && newValue.hasOwnProperty(p))
+                _fields[p](newValue[p]);
+              else
+                _fields[p](undefined);
+            }
+          }
+          result.dirtyFlag.reset();
+        }
+      });
+
+      function has(l) {
+        return function (e) {
+          return l.indexOf(e) !== -1;
+        };
+      }
+
+      function clean(l) {
+        l.subscribe(function () {
+          l.errors.removeAll();
+        });
+      }
+
+      function parseErrors(errors) {
+        errors = errors || {};
+        for (var p in model) {
+          if (model.hasOwnProperty(p)) {
+            _fields[p].errors.removeAll();
+            if (errors.hasOwnProperty(p)) {
+              for (var e in errors[p]) {
+                if (errors[p].hasOwnProperty(e)) {
+                  _fields[p].errors.push(e);
+                }
+              }
+            }
+          }
+        }
+      }
+
+      for (var p in model) {
+        if (model.hasOwnProperty(p)) {
+          if (model[p] === 'date')
+            _fields[p] = koext.observableDate(_value[p]);
+          else if (model[p] === 'number')
+            _fields[p] = koext.observableNumber(_value[p]);
+          else if (model[p] === 'integer')
+            _fields[p] = koext.observableInteger(_value[p]);
+          else
+            _fields[p] = koext.observable(_value[p]);
+          _fields[p].errors = ko.observableArray();
+          _fields[p].errors.has = has(_fields[p].errors);
+          clean(_fields[p]);
+          result[p] = _fields[p];
+        }
+      }
+
+      result.parseErrors = parseErrors;
+      result.dirtyFlag = koext.dirtyFlag(_fields);
+
+      return result;
+    }
+  } // factory
+
+  if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
+      factory(require("knockout"), exports);
+  else if (typeof define === "function" && define["amd"]) {
+      define(["knockout", "exports"], factory);
+  else {
+      factory(ko, ko);
+})();
