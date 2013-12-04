bae43bb4a5099f7b34befa96275b88fb7dea1044
Alex Cornejo <acornejo@gmail.com>
2013-12-03 23:46:39 -0500

    Added chooseFile bindings.

--- knockout.extras.js
+++ knockout.extras.js
@@ -192,12 +192,56 @@
 
       return result;
     };
+
+    ko.bindingHandlers.chooseFile = {
+      init: function (element, valueAccessor, allBindingsAccesor) {
+        var form = document.createElement('form');
+        var input = document.createElement('input');
+        input.type = 'file';
+        input.name = 'file';
+        form.appendChild(input);
+        element.onclick = function () {
+          input.click();
+        };
+        input.onchange = function () {
+          var value = valueAccessor();
+          value(input.files);
+        };
+      }
+    };
+
+    ko.bindingHandlers.chooseFileAsDataURL = {
+      init: function (element, valueAccessor, allBindingsAccesor) {
+        var form = document.createElement('form');
+        var input = document.createElement('input');
+        input.type = 'file';
+        input.name = 'file';
+        form.appendChild(input);
+        element.onclick = function () {
+          input.click();
+        };
+        input.onchange = function () {
+          var value = valueAccessor();
+          if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+              value(e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+          } else {
+            value(undefined);
+          }
+        };
+      }
+    };
+  }
+
   } // factory
 
   if (typeof require === "function" && typeof exports === "object" && typeof module === "object") 
-      factory(require("knockout"), exports);
+    factory(require("knockout"), exports);
   else if (typeof define === "function" && define.amd) 
-      define(["knockout", "exports"], factory);
+    define(["knockout", "exports"], factory);
   else
-      factory(ko, ko);
+    factory(ko, ko);
 })();
