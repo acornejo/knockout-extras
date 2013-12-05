615e28c46b1930dbd27af800e3899d19c4eb6151
Alex Cornejo <acornejo@gmail.com>
2013-12-05 16:47:02 -0500

    Merged upload.

--- knockout.extras.js
+++ knockout.extras.js
@@ -193,70 +193,81 @@
       return result;
     };
 
+    function processFiles(bindings, files) {
+      var reader;
+
+      if (bindings.fileDataURL && ko.isObservable(bindings.fileDataURL)) {
+        if (files && files[0]) {
+          reader = new FileReader();
+          reader.onload = function (e) {
+            bindings.fileDataURL(e.target.result);
+          };
+          reader.readAsDataURL(files[0]);
+        } else 
+          bindings.fileDataURL(undefined);
+      }
+
+      if (bindings.fileArrayBuffer && ko.isObservable(bindings.fileArrayBuffer)) {
+        if (files && files[0]) {
+          reader = new FileReader();
+          reader.onload = function (e) {
+            bindings.fileArrayBuffer(e.target.result);
+          };
+          reader.readAsArrayBuffer(files[0]);
+        } else 
+          bindings.fileArrayBuffer(undefined);
+      }
+
+      if (bindings.fileBinaryString && ko.isObservable(bindings.fileBinaryString)) {
+        if (files && files[0]) {
+          reader = new FileReader();
+          reader.onload = function (e) {
+            bindings.fileBinaryString(e.target.result);
+          };
+          reader.readAsBinaryString(files[0]);
+        } else 
+          bindings.fileBinaryString(undefined);
+      }
+
+      if (bindings.fileText && ko.isObservable(bindings.fileText)) {
+        if (files && files[0]) {
+          reader = new FileReader();
+          reader.onload = function (e) {
+            bindings.fileText(e.target.result);
+          };
+          reader.readAsText(files[0]);
+        } else 
+          bindings.fileText(undefined);
+      }
+    }
+
     ko.bindingHandlers.chooseFile = {
       init: function (element, valueAccessor, allBindingsAccesor) {
+        var bindings = allBindingsAccesor();
         var form = document.createElement('form');
         var input = document.createElement('input');
         input.type = 'file';
         input.name = 'file';
+        if (bindings.fileAccept)
+          input.accept = ko.utils.unwrapObservable(bindings.fileAccept);
+        if (bindings.fileMultiple && ko.utils.unwrapObservable(bindings.fileMultiple))
+          input.multiple = "multiple";
         form.appendChild(input);
         element.onclick = function () {
           input.click();
         };
         input.onchange = function () {
           var value = valueAccessor();
-          value(input.files);
+
+          if (ko.isObservable(value))
+            value(input.files);
+          else
+            processFiles(bindings, input.files);
         };
       },
 
       update: function (element, valueAccessor, allBindingsAccesor) {
-        var files = ko.utils.unwrapObservable(valueAccessor());
-        var bindings = allBindingsAccesor();
-        var reader;
-
-        if (bindings.fileDataURL && ko.isObservable(bindings.fileDataURL)) {
-          if (files && files[0]) {
-            reader = new FileReader();
-            reader.onload = function (e) {
-              bindings.fileDataURL(e.target.result);
-            };
-            reader.readAsDataURL(files[0]);
-          } else 
-            bindings.fileDataURL(undefined);
-        }
-
-        if (bindings.fileArrayBuffer && ko.isObservable(bindings.fileArrayBuffer)) {
-          if (files && files[0]) {
-            reader = new FileReader();
-            reader.onload = function (e) {
-              bindings.fileArrayBuffer(e.target.result);
-            };
-            reader.readAsArrayBuffer(files[0]);
-          } else 
-            bindings.fileArrayBuffer(undefined);
-        }
-
-        if (bindings.fileBinaryString && ko.isObservable(bindings.fileBinaryString)) {
-          if (files && files[0]) {
-            reader = new FileReader();
-            reader.onload = function (e) {
-              bindings.fileBinaryString(e.target.result);
-            };
-            reader.readAsBinaryString(files[0]);
-          } else 
-            bindings.fileBinaryString(undefined);
-        }
-
-        if (bindings.fileText && ko.isObservable(bindings.fileText)) {
-          if (files && files[0]) {
-            reader = new FileReader();
-            reader.onload = function (e) {
-              bindings.fileText(e.target.result);
-            };
-            reader.readAsText(files[0]);
-          } else 
-            bindings.fileText(undefined);
-        }
+        processFiles(allBindingsAccesor(), ko.utils.unwrapObservable(valueAccessor()));
       }
     };
 
